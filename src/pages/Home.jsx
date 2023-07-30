d329c6273d31f6a57489f6aad611b92c65827a74
sinans <svrsinan06@gmail.com>
2023-07-30 16:24:03 +0300

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([])
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+
+  const getTutorials = async () => {
+    try {
+      const data = await axios(BASE_URL)
+      setTutorials(data.data)
+    } catch (error) {
+      console.log(error)
+    }
+    const data = await axios(BASE_URL)
+    setTutorials(data.data)
+  }
+
+  useEffect(() => {
+    getTutorials()
+  }, [])
+
+  console.log(tutorials)
+
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials={tutorials}/>
     </>
   );
 };
